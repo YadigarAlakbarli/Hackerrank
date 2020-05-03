fb3ac8fb3b89657d2cc7ec8e1edeb13d93d2fdf3
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-03 15:25:29 +0400

    Task-1 added

---
+++ src/com/company/tasks/Task_1.java
@@ -0,0 +1,13 @@
+package com.company.tasks;
+
+public class Task_1 {
+    /*Welcome to  the world of Java!In this challenge,we practice printing to stdout .
+  The code stubs in your editor declare a Solution class and a main method.
+  Complete the main method by copyingthe two lines of code below   and pasting
+  them insidethe bodyof your main method*/
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
