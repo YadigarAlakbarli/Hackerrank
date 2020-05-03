92e967b4949175de8c8c076aab1f6af2c0159ad1
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-03 19:09:08 +0400

    Task-8 added

---
+++ src/com/company/tasks/Task_8.java
@@ -0,0 +1,29 @@
+package com.company.tasks;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+
+                if (x >= -Math.pow(2, 16) && x < Math.pow(2, 16) - 1) System.out.println("* short");
+
+                if (x >= -Math.pow(2, 32) && x < Math.pow(2, 32) - 1) System.out.println("* int");
+
+                if (x >= -Math.pow(2, 64) && x < Math.pow(2, 64) - 1) System.out.println("* long");
+
+
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
