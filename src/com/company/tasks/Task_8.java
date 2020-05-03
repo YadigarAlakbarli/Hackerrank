fcaef1944b593f6c1aebcf235cab3b7f3abb0c74
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-03 21:47:11 +0400

    Task-9 added

--- src/com/company/tasks/Task_8.java
+++ src/com/company/tasks/Task_8.java
@@ -5,26 +5,31 @@ import java.util.Scanner;
 public class Task_8 {
     //Given an input integer,
     // you must determine which primitive data types are capable of properly storing that input
-    
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        int t=sc.nextInt();
 
-        for (int i = 0; i < t; i++) {
+        for(int i=0;i<t;i++)
+        {
 
-            try {
-                long x = sc.nextLong();
-                System.out.println(x + " can be fitted in:");
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
 
-                if (x >= -Math.pow(2, 16) && x < Math.pow(2, 16) - 1) System.out.println("* short");
+                if (x >= -32768 && x <= 32767 ) System.out.println("* short");
 
-                if (x >= -Math.pow(2, 32) && x < Math.pow(2, 32) - 1) System.out.println("* int");
+                if (x >= -2147483648 && x <= 2147483647 ) System.out.println("* int");
 
-                if (x >= -Math.pow(2, 64) && x < Math.pow(2, 64) - 1) System.out.println("* long");
+                if (x >= -9223372036854775808l && x <= 9223372036854775807l ) System.out.println("* long");
 
-
-            } catch (Exception e) {
-                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
             }
 
         }
