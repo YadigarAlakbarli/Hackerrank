88f0cde642832d4ce8fdc918ad4a841cbf775a9d
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-03 15:28:16 +0400

    Task-2 added

---
+++ src/com/company/tasks/Task_2.java
@@ -0,0 +1,22 @@
+package com.company.tasks;
+
+import java.util.Scanner;
+
+public class Task_2 {
+/*
+    In this challenge, you must read integers from stdin and then print them to stdout.
+    Each integer must be printed on a new line. To make the problem a little easier,
+     a portion of the code is provided for you in the editor below.
+*/
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
