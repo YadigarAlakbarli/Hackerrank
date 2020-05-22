1b9c312192abeee9c8b5dc3b06cb451e447e84a1
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-22 17:46:59 +0400

    Task 15 added

--- src/com/company/tasks/Task_15.java
+++ src/com/company/tasks/Task_15.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task_15 {
+    //Java Substring Comparisons
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
