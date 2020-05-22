bc69f6aea0a40b2cf3fe1d3f8e427ec39f6dd551
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-22 15:13:50 +0400

    Task 13 added

---
+++ src/com/company/tasks/Task_13.java
@@ -0,0 +1,23 @@
+package com.company.tasks;
+
+import java.util.Scanner;
+
+public class Task_13 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+
+        System.out.println(A.length()+B.length());
+        int n=A.compareTo(B);
+
+        if (n <= 0)
+            System.out.println("No");
+         else
+            System.out.println("Yes");
+
+        System.out.println(A.substring(0,1).toUpperCase()+A.substring(1) +
+                           " "+B.substring(0,1).toUpperCase()+B.substring(1));
+    }
+}
