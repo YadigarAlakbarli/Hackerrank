ae407454a1f4ac2f4c94a024e06acbc9dca45470
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-22 16:21:44 +0400

    Task 15 added

---
+++ src/com/company/tasks/Task_15.java
@@ -0,0 +1,34 @@
+package com.company.tasks;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task_15 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+
+    }
+
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0,k);
+        String largest = s.substring(0,k);
+
+
+        for (int i = 0; i <s.length()-k+1 ; i++) {
+            String current = s.substring(i, i + k);
+
+            if(smallest.compareTo(current) > 0) smallest = current;
+            if(largest.compareTo(current) < 0) largest = current;
+        }
+
+        return smallest + "\n" + largest;
+    }
+}
