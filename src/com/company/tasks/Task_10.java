751c3660c0b3a97d11f70c5c8acdda3aa66762e1
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-22 14:08:28 +0400

    Task 10 added

---
+++ src/com/company/tasks/Task_10.java
@@ -0,0 +1,24 @@
+package com.company.tasks;
+
+import java.util.Scanner;
+
+public class Task_10 {
+    private static int B,H;
+    private static boolean flag=true;
+    private static Scanner scanner;
+static  {
+    scanner=new Scanner(System.in);
+    int B=scanner.nextInt();
+    int H=scanner.nextInt();
+   if (B<0 && H<0){
+        new Exception("Breadth and height must be positive");
+   }
+
+}
+    public static void main(String[] args) {
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+    }
+}
