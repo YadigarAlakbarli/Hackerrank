4fdf057da23e0f56ace1b6c45a191228a165a653
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-24 13:12:45 +0400

    Task 21 added

--- src/com/company/tasks/Task_20.java
+++ src/com/company/tasks/Task_20.java
@@ -20,5 +20,6 @@ public class Task_20 {
 }
 
 class MyRegex {
-    public String pattern = "a&&b";
+    String from0To255 = "(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])";
+    public String pattern = String.format("%s.%s.%s.%s", from0To255, from0To255, from0To255, from0To255);
 }
