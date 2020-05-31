0b274e3b61f445e51d6ce46d9ddee1e75d1371be
Yadigar Elekberli <yadigar.alakbarli@gmail.com>
2020-05-31 15:30:28 +0400

    Task 28 added

---
+++ src/com/company/tasks/interfaces/Task_28.java
@@ -0,0 +1,31 @@
+package com.company.tasks.interfaces;
+
+public class Task_28 {
+    //Java Method Overriding 2 (Super Keyword)
+
+    public static void main(String[] args) {
+        MotorCycle M=new MotorCycle();
+    }
+}
+
+
+class BiCycle{
+    String define_me(){
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle{
+    String define_me(){
+        return "a cycle with an engine.";
+    }
+
+    MotorCycle(){
+        System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+        String temp=super.define_me(); //Fix this line
+
+        System.out.println("My ancestor is a cycle who is "+ temp );
+    }
+
+}
